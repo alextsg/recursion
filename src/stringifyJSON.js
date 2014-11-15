be0d4714f9ee7f0f00a62c19bb030c7770410d7e
Alex Tseung <alextsg@gmail.com>
2014-11-15 14:16:31 -0800

    Completed "stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  	var result = '';
+  	if (typeof(obj) == "string"){
+  		result += '"'+obj+'"';
+  	}
+  	else if (Array.isArray(obj)){
+  		result += "[";
+  		for (var i=0;i<obj.length;i++){
+  			result += stringifyJSON(obj[i])+',';
+  		}
+  		if (result[result.length-1] == ","){
+  		result = result.substring(0,result.length - 1);
+  		}
+  		result += "]";
+  	}
+  	else if(Object.prototype.toString.call(obj) === '[object Object]'){
+  		result += "{";
+  		for (var key in obj){
+  			if (typeof(obj[key])!=="function" && typeof(obj[key])!=="undefined") {
+  				result += stringifyJSON(key)+':'+stringifyJSON(obj[key])+',';
+  			}
+  		}
+  		if (result[result.length-1] == ","){
+  		result = result.substring(0,result.length - 1);
+  		}
+  		result += "}";
+  	}
+  	else {
+  		result += obj;
+  	}
+  	return result;
 };
